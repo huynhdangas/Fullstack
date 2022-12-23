f58ea6d9c71c369a3d6dbfd23ac631c35ed95918
HDang <huynhnguyendang12@gmail.com>
2022-12-23 10:41:08 +0700

    Add table markdown, doctorcontroller

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -37,6 +37,128 @@ let getTopDoctorHome = (limit) => {
     });
 };
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: "R2" },
+                attributes: {
+                    exclude: ["password", "image"],
+                },
+            });
+            resolve({
+                errCode: 0,
+                data: doctors,
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let saveDetailInforDoctor = (inputData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (
+                !inputData.doctorId ||
+                !inputData.contentHTML ||
+                !inputData.contentMarkdown ||
+                !inputData.action
+            ) {
+                resolve({
+                    errCode: -1,
+                    errMessage: "Missing parameters",
+                });
+            } else {
+                if (inputData.action === "CREATE") {
+                    await db.Markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId,
+                    });
+                } else if (inputData.action === "EDIT") {
+                    let doctorMarkdown = await db.Markdown.findOne({
+                        where: { doctorId: inputData.doctorId },
+                        raw: false,
+                    });
+                    if (doctorMarkdown) {
+                        doctorMarkdown.contentHTML = inputData.contentHTML;
+                        doctorMarkdown.contentMarkdown =
+                            inputData.contentMarkdown;
+                        doctorMarkdown.description = inputData.description;
+
+                        await doctorMarkdown.save();
+                    }
+                }
+
+                resolve({
+                    errCode: 0,
+                    errMessage: "Save infor doctor success",
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let getDetailDoctorById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing parameter",
+                });
+            } else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: id,
+                    },
+                    attributes: {
+                        exclude: ["password"],
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: [
+                                "description",
+                                "contentHTML",
+                                "contentMarkdown",
+                            ],
+                        },
+                        {
+                            model: db.Allcode,
+                            as: "positionData",
+                            attributes: ["valueEn", "valueVi"],
+                        },
+                    ],
+                    raw: false,
+                    nest: true,
+                });
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, "base64").toString(
+                        "binary"
+                    );
+                }
+                if (!data) {
+                    data = {};
+                }
+                resolve({
+                    errCode: 0,
+                    data: data,
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    saveDetailInforDoctor: saveDetailInforDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 };
